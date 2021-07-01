e7cbde61fea7766a780b99777d1ce2f35cb8dd26
St0nks ✧ treblA <73238186+quytrungg@users.noreply.github.com>
2021-07-01 21:45:42 +0700

    2nd attempt

--- ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/header.h
+++ ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/header.h
@@ -6,8 +6,27 @@
 #include <sstream>
 #include <time.h>
 
+void swap_element(int a, int b);
+
+void bubble_sort(int* a, int n);
+void bubble_sort_compare(int* a, int n, int& count_compare);
+void selection_sort(int* a, int n);
+void selection_sort_compare(int* a, int n, int& count_compare);
+void insertion_sort(int* a, int n);
+void insertion_sort_compare(int* a, int n, int& count_compare);
+void shaker_sort(int* a, int n);
+void shaker_sort_compare(int* a, int n, int& count_compare);
+void shell_sort(int* a, int n);
+void shell_sort_comparision(int* a, int n, int& count_compare);
 
 
+template <class T>
+void HoanVi(T& a, T& b);
+void GenerateRandomData(int a[], int n); 
+void GenerateSortedData(int a[], int n);
+void GenerateReverseData(int a[], int n);
+void GenerateNearlySortedData(int a[], int n);
+void GenerateData(int a[], int n, int dataType);
 
 #endif // !HEADER_H
 
--- ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/sort.cpp
+++ ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/sort.cpp
@@ -121,7 +121,7 @@ void shaker_sort_compare(int* a, int n, int& count_compare) {
 	//a = new int[n];
 	count_compare = 0;
 	int left = 0, right = n - 1, idx;
-	while (++count_compare && left < right) {
+	while (++count_compare&& left < right) {
 		for (int i = right; ++count_compare && i > left; i--) {
 			if (++count_compare && a[i - 1] > a[i]) {
 				swap_element(a[i], a[i - 1]);
@@ -130,7 +130,7 @@ void shaker_sort_compare(int* a, int n, int& count_compare) {
 		}
 		left = idx;
 		for (int i = left; ++count_compare && i < right; i++) {
-			if (++count_compare && a[i + 1] < a[i]) {
+			if (++count_compare&& a[i + 1] < a[i]) {
 				swap_element(a[i], a[i + 1]);
 				idx = i;
 			}
@@ -141,12 +141,84 @@ void shaker_sort_compare(int* a, int n, int& count_compare) {
 
 //5. Shell sort
 void shell_sort(int* a, int n) {
-
+	//a = new int[n];
+	for (int interval = n / 2; interval > 0; interval = interval / 2) {
+		for (int i = interval; i < n; i++) {
+			int temp = a[i], j = i;
+			for (j = i; j >= interval && a[j - interval] > temp; j -= interval) {
+				a[j] = a[j - interval];
+			}
+			a[j] = temp;
+		}
+	}
 }
 
 //Shell sort with comparision
 void shell_sort_comparision(int* a, int n, int& count_compare) {
+	//a = new int[n];
+	count_compare = 0;
+	for (int interval = n / 2; ++count_compare && interval > 0; interval = interval / 2) {
+		for (int i = interval; ++count_compare && i < n; i++) {
+			int temp = a[i], j = i;
+			for (j = i; ++count_compare && j > interval && ++count_compare && a[j - interval] > temp; j -= interval) {
+				a[j] = a[j - interval];
+			}
+			a[j] = temp;
+		}
+	}
+}
+
+//6. Heap sort
+
+
+//7. Merge sort
+void merge_sort(int* a, int left, int right) {
+	if (left < right) {
+		int mid = (left + (right - 1)) / 2;
+		merge_sort(a, left, mid);
+		merge_sort(a, mid + 1, right);
+		merge(a, left, mid, right);
+	}
+}
+
+void merge(int* a, int left, int mid, int right) {
+	int n1 = mid - left + 1, n2 = right - mid;
+	int* temp1 = new int[n1];
+	int* temp2 = new int[n2];
+	for (int i = 0; i < right; i++) {
+		if (i <= n1) {
+			temp1[i] = a[i];
+		}
+		else {
+			temp2[i - n1] = a[i];
+		}
+	}
+	int i = 0, j = 0, idx = left;
+	while (i < n1 && j < n2) {
+		if (temp1[i] <= temp2[j]) {
+			a[idx] = temp1[i];
+			i++;
+		}
+		else {
+			a[idx] = temp2[j];
+			j++;
+		}
+		idx++;
+	}
+	while (i < n1) {
+		a[idx] = temp1[i];
+	}
+	while (j < n2) {
+		a[idx] = temp2[j];
+	}
+}
+
+//Merge sort with comparision
+void merge_sort_compare(int* a, int left, int right, int& count_compare) {
 
 }
 
+void merge_compare(int* a, int left, int right, int& count_compare) {
+
+}
 
