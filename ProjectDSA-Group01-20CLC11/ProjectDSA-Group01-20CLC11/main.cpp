852539c8cc0e301f9d39b12b731faa5f670b2a29
St0nks ✧ treblA <73238186+quytrungg@users.noreply.github.com>
2021-07-02 17:54:56 +0700

    fixed

--- ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/main.cpp
+++ ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/main.cpp
@@ -6,5 +6,23 @@ int main(int argc, char* argv[]) {
 	{
 		algorithmMode(argc, argv, a);
 	}
+	//int* a, n, datatype, count_compare = 0;
+	//std::cout << "Enter n: ";
+	//std::cin >> n;
+	//std::cout << "Enter data type: ";
+	//std::cin >> datatype;
+	//a = new int[n];
+	//GenerateData(a, n, datatype);
+	//clock_t start, end;
+	//start = clock();
+	////insert sorting algorithm
+	//merge_sort(a, 0, n - 1);
+	//end = clock();
+	////output time
+	//std::cout << "Time: " << end - start << " ms\n";
+	////insert comparison sorting algorithm
+	//merge_sort_compare(a, 0, n - 1, count_compare);
+	////output comparison
+	//std::cout << "Comparison: " << count_compare;
 	return 0;
 }
