895e615f4415db2a80b9bcb7d839aa64a3488e5c
St0nks ✧ treblA <73238186+quytrungg@users.noreply.github.com>
2021-07-10 12:33:03 +0700

    Final

--- ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/header.h
+++ ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/header.h
@@ -1,16 +1,16 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-//----------LIBRARY----------
+//--------------------LIBRARY--------------------
 #include <iostream>
 #include <fstream>
+#include <ctype.h> // to differentiate between number and char
 #include <sstream>
 #include <time.h>
-#include <ctype.h> // to differentiate between number and char
-#include <string> // because... reasons
+#include <string>
 #include <cstring>
 
-//----------DEFINITION----------
+//--------------------DEFINITION--------------------
 #define selectionsort 1
 #define insertionsort 2
 #define bubblesort 3
@@ -32,55 +32,55 @@
 #define comparison 2
 #define both 3
 
-//----------BUBBLE SORT----------
+//--------------------BUBBLE SORT--------------------
 void bubble_sort(int* a, int n);
 void bubble_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------SELECTION SORT----------
+//--------------------SELECTION SORT--------------------
 void selection_sort(int* a, int n);
 void selection_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------INSERTION SORT----------
+//--------------------INSERTION SORT--------------------
 void insertion_sort(int* a, int n);
 void insertion_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------SHAKER SORT----------
+//--------------------SHAKER SORT--------------------
 void shaker_sort(int* a, int n);
 void shaker_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------SHELL SORT----------
+//--------------------SHELL SORT--------------------
 void shell_sort(int* a, int n);
 void shell_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------HEAP SORT----------
+//--------------------HEAP SORT--------------------
 void heapify_sort(int* a, int n, int i);
 void heap_sort(int* a, int n);
 void heapify_sort_compare(int* a, int n, int i, unsigned long long & count_compare);
 void heap_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------MERGE SORT----------
+//--------------------MERGE SORT--------------------
 void merge_sort(int* a, int left, int right);
 void merge(int* a, int left, int mid, int right);
 void merge_sort_compare(int* a, int left, int right, unsigned long long & count_compare);
 void merge_compare(int* a, int left, int mid, int right, unsigned long long & count_compare);
 
-//----------QUICK SORT----------
+//--------------------QUICK SORT--------------------
 void quick_sort(int* a, int left, int right);
 void quick_sort_compare(int* a, int left, int right, unsigned long long & count_compare);
 
-//----------COUNTING SORT----------
+//--------------------COUNTING SORT--------------------
 void counting_sort(int* a, int n);
 void counting_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------RADIX SORT----------
+//--------------------RADIX SORT--------------------
 void radix_sort(int* a, int n);
 void radix_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------FLASH SORT----------
+//--------------------FLASH SORT--------------------
 void flash_sort(int* a, int n);
 void flash_sort_compare(int* a, int n, unsigned long long & count_compare);
 
-//----------DATA GENERATOR----------
+//--------------------DATA GENERATOR--------------------
 template <class T>
 void HoanVi(T& a, T& b);
 void GenerateRandomData(int a[], int n); 
@@ -89,13 +89,13 @@ void GenerateReverseData(int a[], int n);
 void GenerateNearlySortedData(int a[], int n);
 void GenerateData(int a[], int n, int dataType);
 
-//----------FILE----------
+//--------------------FILE--------------------
 void outputFile(std::string, int*, int);
 int* inputFile(std::string filename, int n);
 int inputSize(std::string filename);
 int* copyFile(int* a, int n);
 
-//----------RUNTIME & COMPARISON----------
+//--------------------RUNTIME & COMPARISON--------------------
 int selectionSortRuntime(int* a, int n);
 int insertionSortRuntime(int* a, int n);
 int bubbleSortRuntime(int* a, int n);
@@ -110,10 +110,10 @@ int flashSortRuntime(int* a, int n);
 int runTime(int algorithm, int* a, int n);
 unsigned long long compCount(int algorithm, int* a, int n);
 
-//----------SUB FUNCTION----------
+//--------------------SUB FUNCTION--------------------
 bool check_number(char* argv);
 
-//----------COMMAND LINE----------
+//--------------------COMMAND LINE--------------------
 void command_1(char* argv[], int algorithm, int outmode, std::string filename);
 void command_2(char* argv[], int algorithm, int size, int order, int outmode);
 void command_3(char* argv[], int algorithm, int size, int outmode);
--- ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/sort.cpp
+++ ProjectDSA-Group01-20CLC11/ProjectDSA-Group01-20CLC11/sort.cpp
@@ -587,3 +587,140 @@ void flash_sort_compare(int* a, int n, unsigned long long & count_compare) {
 	insertion_sort_compare(a, n, count_compare);
 }
 
+//1. Selection sort runtime
+int selectionSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	selection_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//2. Insertion sort runtime 
+int insertionSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	insertion_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//3. Bubble sort runtime
+int bubbleSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	bubble_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//4 Shaker sort runtime
+int shakerSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	shaker_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//5. Shell sort runtime
+int shellSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	shell_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//6. Heap sort runtime
+int heapSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	heap_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//7. Merge sort runtime
+int mergeSortRuntime(int* a, int left, int right) {
+	clock_t start, end;
+	start = clock();
+	merge_sort(a, left, right);
+	end = clock();
+	return end - start;
+}
+
+//8 Quick sort runtime
+int quickSortRuntime(int* a, int left, int right) {
+	clock_t start, end;
+	start = clock();
+	quick_sort(a, left, right);
+	end = clock();
+	return end - start;
+}
+
+//9. Counting sort runtime
+int countingSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	counting_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//10. Radix sort runtime
+int radixSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	radix_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//11. Flash sort runtime
+int flashSortRuntime(int* a, int n) {
+	clock_t start, end;
+	start = clock();
+	flash_sort(a, n);
+	end = clock();
+	return end - start;
+}
+
+//Runtime
+int runTime(int algorithm, int* a, int n) {
+	switch (algorithm)
+	{
+	case 1: return selectionSortRuntime(a, n);
+	case 2: return insertionSortRuntime(a, n);
+	case 3: return bubbleSortRuntime(a, n);
+	case 4: return shakerSortRuntime(a, n);
+	case 5: return shellSortRuntime(a, n);
+	case 6: return heapSortRuntime(a, n);
+	case 7: return mergeSortRuntime(a, 0, n - 1);
+	case 8: return quickSortRuntime(a, 0, n - 1);
+	case 9: return countingSortRuntime(a, n);
+	case 10: return radixSortRuntime(a, n);
+	case 11: return flashSortRuntime(a, n);
+	default: return 0;
+	}
+}
+
+//Comparison
+unsigned long long compCount(int algorithm, int* a, int n) {
+	unsigned long long count_compare = 0;
+	switch (algorithm)
+	{
+	case 1: selection_sort_compare(a, n, count_compare); return count_compare;
+	case 2: insertion_sort_compare(a, n, count_compare); return count_compare;
+	case 3: bubble_sort_compare(a, n, count_compare); return count_compare;
+	case 4: shaker_sort_compare(a, n, count_compare); return count_compare;
+	case 5: shell_sort_compare(a, n, count_compare); return count_compare;
+	case 6: heap_sort_compare(a, n, count_compare); return count_compare;
+	case 7: merge_sort_compare(a, 0, n - 1, count_compare); return count_compare;
+	case 8: quick_sort_compare(a, 0, n - 1, count_compare); return count_compare;
+	case 9: counting_sort_compare(a, n, count_compare); return count_compare;
+	case 10: radix_sort_compare(a, n, count_compare); return count_compare;
+	case 11: flash_sort_compare(a, n, count_compare); return count_compare;
+	default: return -1;
+	}
+}
